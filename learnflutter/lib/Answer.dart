f90daa7c9e60f21311e8d36866ba0e3a49907279
Alvin-Mwangi-W <alvin.wacira.m@gmail.com>
2022-08-24 12:19:25 +0300

    Wrapped in padding Widget

--- learnflutter/lib/Answer.dart
+++ learnflutter/lib/Answer.dart
@@ -2,17 +2,22 @@ import 'package:flutter/material.dart';
 
 class Answer extends StatelessWidget {
   final VoidCallback selectAnswer;
+  final String answerText;
 
-  Answer(this.selectAnswer);
+  Answer(this.selectAnswer, this.answerText);
 
   @override
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
-      child: RaisedButton(
-        color: Colors.blue,
-        child: Text('Answer 1'),
-        onPressed: selectAnswer,
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(0, 0, 0, 10),
+        child: RaisedButton(
+          color: Colors.blue,
+          textColor: Colors.white,
+          child: Text(answerText),
+          onPressed: selectAnswer,
+        ),
       ),
     );
   }
