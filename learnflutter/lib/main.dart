97bd72cb41eadf080dc3a3b4f4f23d6c67b78ab5
Alvin-Mwangi-W <alvin.wacira.m@gmail.com>
2022-08-26 18:32:13 +0300

    fixed some errors

--- learnflutter/lib/main.dart
+++ learnflutter/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
-import 'quiz.dart';
+import './quiz.dart';
+import './result.dart';
 
 void main() {
   runApp(MyApp());
@@ -19,26 +20,50 @@ class _MyAppState extends State<MyApp> {
     const _questions = [
       {
         'questionText': 'What\'s your favorite color?',
-        'answers': ['Black', 'White', 'Blue', 'Yellow']
+        'answers': [
+          {'text': 'Black', 'score': 5},
+          {'text': 'White', 'score': 1},
+          {'text': 'Blue', 'score': 2},
+          {'text': 'Yellow', 'score': 8}
+        ]
       },
       {
         'questionText': 'What\'s your favorite animal',
-        'answers': ['Dog', 'Cat', 'Owl', 'Sloth']
+        'answers': [
+          {'text': 'Dog', 'score': 3},
+          {'text': 'Cat', 'score': 10},
+          {'text': 'Owl', 'score': 8},
+          {'text': 'Sloth', 'score': 2}
+        ]
       },
       {
         'questionText': 'What\'s your favorite Meal',
-        'answers': ['Githeri', 'Chapo', 'Chicken', 'Fish']
+        'answers': [
+          {'text': 'Githeri', 'score': 4},
+          {'text': 'Chapo', 'score': 6},
+          {'text': 'fish', 'score': 8},
+          {'text': 'Chicken', 'score': 2}
+        ]
       },
       {
         'questionText': 'What\'s your favorite Lecturer',
-        'answers': ['None', 'Wainanina', 'Kungu', 'Wanjiku']
+        'answers': [
+          {'text': 'Wainaina', 'score': 1},
+          {'text': 'Fred', 'score': 8},
+          {'text': 'Wanjiku', 'score': 2},
+          {'text': 'None', 'score': 10}
+        ]
       },
     ];
     var _questionIndex = 0;
 
-    void _answerQuestion() {
+    var totalScore = 0;
+
+    void _answerQuestion(int score) {
+      totalScore += score;
+
       if (_questionIndex < _questions.length) {
-        print("We have more qiestions");
+        print("We have more questions");
       }
       setState(() {
         _questionIndex = _questionIndex + 1;
@@ -49,19 +74,16 @@ class _MyAppState extends State<MyApp> {
 
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('My first App'),
-        ),
-        body: _questionIndex < _questions.length
-            ? Quiz(
-                answerQuestion: _answerQuestion,
-                questions: _questions,
-                questionIndex: _questionIndex,
-              )
-            : Center(
-                child: Text("You did it!!"),
-              ),
-      ),
+          appBar: AppBar(
+            title: Text('Personality App'),
+          ),
+          body: _questionIndex < _questions.length
+              ? Quiz(
+                  answerQuestion: _answerQuestion,
+                  questions: _questions,
+                  questionIndex: _questionIndex,
+                )
+              : Result()),
     );
   }
 }
